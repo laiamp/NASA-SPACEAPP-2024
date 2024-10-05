fbb7a70ee17976fc1b07557af888695f3d25d60c
alberto0444 <abecharabordes@gmail.com>
2024-10-05 11:53:58 +0000

    Graph pochisimo

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,12 @@
-    import React from 'react';
+import React from 'react';
+import ScatterPlot from './home/plot';
 
 const Home = () => {
   return (
     <div>
       <h2>Inicio</h2>
       <p>Bienvenido a la página de inicio.</p>
+      <ScatterPlot/>
     </div>
   );
 };
